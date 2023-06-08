f0063ad5db9d0705849f237b21d1270c65baff57
kakhiG <49832276+kakhiG@users.noreply.github.com>
2023-06-08 23:13:57 +0400

    Migrate on startup

--- AdminWebApp/Program.cs
+++ AdminWebApp/Program.cs
@@ -41,4 +41,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+await app.Services.GetRequiredService<AppDbContext>().Database.MigrateAsync();
+
 app.Run();
